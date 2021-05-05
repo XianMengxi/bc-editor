210aa9472ad56331481ebd184fc99c122584e67a
Zhou-bei-jin <75028066+Zhou-bei-jin@users.noreply.github.com>
2021-05-05 20:51:39 +0800

    Delete include directory

--- include/code.h
+++
@@ -1,7 +0,0 @@
-#include"define.h"
-void code (Hnode *hnode,Cursornode *cursornode,int flag);
-int complie(Hnode* hnode,Cursornode *cursornode,int flag,int flag_c,char ch1,char ch2);
-
-void chinese(Hnode * hnode,Cursornode *cursornode,int h,int *color);
-void ziti_set(int i);
-int ziti_set2(void);
--- include/edit.h
+++
@@ -1,8 +0,0 @@
-#ifndef _EDIT_H
-#define _EDIT_H
-#pragma once
-#include"function.h"
-#include"define.h"
-#include"CONIO.H"
-Hnode* Add(Hnode*hnode, Cursornode *cursornode, int key,int *saveflags,int *pcolor);  //±à¼­
-#endif
